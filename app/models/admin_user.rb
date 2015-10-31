91131871e77d040a9b57b71a1db47e913357f72f
amronrails <amre.abdelhady@gmail.com>
2015-11-01 00:21:45 +0200

    ASC2

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,7 @@ class AdminUser < ActiveRecord::Base
 	#associations
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "50x50#" }, default_url: "/images/:style/missing.png"
 	has_secure_password
-	scope :sorted, lambda{ order("brands.id ASC") }
+	scope :sorted, lambda{ order("admin_users.id ASC") }
 	#validation
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
