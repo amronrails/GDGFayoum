e42755987846b8a4768eb20841e6811076270281
amronrails <amre.abdelhady@gmail.com>
2015-11-01 00:19:53 +0200

    ASC

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,6 +2,7 @@ class AdminUser < ActiveRecord::Base
 	#associations
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "50x50#" }, default_url: "/images/:style/missing.png"
 	has_secure_password
+	scope :sorted, lambda{ order("brands.id ASC") }
 	#validation
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
