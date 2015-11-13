8d414d9b4e9995029c747322ffb6e57edac4a1f4
amronrails <amre.abdelhady@gmail.com>
2015-11-13 17:16:08 +0200

    paper

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,16 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # set :keep_releases, 5
 
 namespace :deploy do
+desc "build missing paperclip styles"
+  task :build_missing_paperclip_styles do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "paperclip:refresh:missing_styles"
+        end
+      end
+    end
+  end
 
   desc 'Restart application'
   task :restart do
@@ -46,5 +56,7 @@ namespace :deploy do
   end
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
+  after("deploy:compile_assets", "deploy:build_missing_paperclip_styles")
+
 end
 
